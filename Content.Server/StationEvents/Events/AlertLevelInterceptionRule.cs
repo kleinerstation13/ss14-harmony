347f58fe1c118b956d5477dd9ae06466d0085322
halycon <filip.grzella@gmail.com>
2025-07-05 04:42:27 +0200

    adds extended early warning

--- Content.Server/StationEvents/Events/AlertLevelInterceptionRule.cs
+++ Content.Server/StationEvents/Events/AlertLevelInterceptionRule.cs
@@ -15,8 +15,8 @@ public sealed class AlertLevelInterceptionRule : StationEventSystem<AlertLevelIn
 
         if (!TryGetRandomStation(out var chosenStation))
             return;
-        if (_alertLevelSystem.GetLevel(chosenStation.Value) != "green")
-            return;
+//      if (_alertLevelSystem.GetLevel(chosenStation.Value) != "green") // harmony
+//          return;                                                     // harmony
 
         _alertLevelSystem.SetLevel(chosenStation.Value, component.AlertLevel, true, true, true);
     }
